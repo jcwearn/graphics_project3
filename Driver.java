a39bf9a680ba7b045a501c70f9df116fe34287a0
Jackson Wearn <jacksonwearn@gmail.com>
2014-10-19 22:25:19 -0400

    Add code from BresenhamLineScan

--- Driver.java
+++ Driver.java
@@ -10,6 +10,25 @@ public class Driver {
 
 	 BresenhamLineScan panel = new BresenhamLineScan(width, height);
 
+	 DataLine line1 = new DataLine(10,10,10,100);
+	 DataLine line2 = new DataLine(10,10,100,10);
+	 DataLine line3 = new DataLine(10,10,100,100);
+	 DataLine line4 = new DataLine(100,100,150,50);
+	 DataLine line5 = line4.basicTranslate(50,50);
+	 DataLine line6 = line5.basicScale(2,1.5);
+	 DataLine line7 = line6.basicRotate(30);
+	 //drawLine(line1);
+	 //horizontal
+	 //drawLine(line2);
+	 //diagonal pos
+	 //drawLine(line3);
+	 //diagonal neg
+	 panel.drawLine(line4);
+	 panel.drawLine(line5);
+	 panel.drawLine(line6);
+	 panel.drawLine(line7);
+	 DataLine.outputLines(panel.getDatalines(), panel.getNumOfDatalines());
+	
 	 frame.add(panel);
 	 frame.pack();
 	 frame.setVisible(true);
