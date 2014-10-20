c08166e8a19d479179647ffea20fe7c3c00d309d
Jackson Wearn <jacksonwearn@gmail.com>
2014-10-19 22:42:38 -0400

    Make text based menu

--- Driver.java
+++ Driver.java
@@ -1,13 +1,13 @@
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import java.util.Scanner;
 
 public class Driver {
      public static void main(String[] args) {
          int width = 640;
          int height = 480;
-	 JFrame frame = new JFrame("Direct draw demo");
-	
-
+	 JFrame frame = new JFrame("Project 3");
+	 Scanner scan = new Scanner(System.in);
 	 BresenhamLineScan panel = new BresenhamLineScan(width, height);
 
 	 DataLine line1 = new DataLine(10,10,10,100);
@@ -27,7 +27,45 @@ public class Driver {
 	 panel.drawLine(line5);
 	 panel.drawLine(line6);
 	 panel.drawLine(line7);
-	 DataLine.outputLines(panel.getDatalines(), panel.getNumOfDatalines());
+
+	 System.out.println("What would you like to do?");
+	 System.out.println("1: Basic Translation");
+	 System.out.println("2: Basic Scale");
+	 System.out.println("3: Basic Rotate");
+	 System.out.println("4: Concatenate");
+	 System.out.println("5: Apply Transformation");
+	 System.out.println("6: Viewport-spec");
+	 System.out.println("7: Display Pixels");
+	 System.out.println("8: Input Lines");
+	 System.out.println("9: Output Lines");
+	 System.out.println("10: Exit");
+
+	 int input = scan.nextInt();
+	 switch(input) {
+	 case 1:
+	     break;
+	 case 2:
+	     break;
+	 case 3:
+	     break;
+	 case 4:
+	     break;
+	 case 5:
+	     break;
+	 case 6:
+	     break;
+	 case 7:
+	     break;
+	 case 8:
+	     break;
+	 case 9:
+	     DataLine.outputLines(panel.getDatalines(), panel.getNumOfDatalines());
+	     break;
+	 case 10:
+	     break;
+	 default:
+	     System.out.println("Invalid input.");
+	 }
 	
 	 frame.add(panel);
 	 frame.pack();
