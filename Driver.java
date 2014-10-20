d35059b1c9bb9a9532b12e90ae221a68926de81a
Jackson Wearn <jacksonwearn@gmail.com>
2014-10-19 22:48:49 -0400

    Add endless while loop

--- Driver.java
+++ Driver.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Driver {
      public static void main(String[] args) {
+	 Boolean isRunning = true;
          int width = 640;
          int height = 480;
 	 JFrame frame = new JFrame("Project 3");
@@ -28,50 +29,55 @@ public class Driver {
 	 panel.drawLine(line6);
 	 panel.drawLine(line7);
 
-	 System.out.println("What would you like to do?");
-	 System.out.println("1: Basic Translation");
-	 System.out.println("2: Basic Scale");
-	 System.out.println("3: Basic Rotate");
-	 System.out.println("4: Concatenate");
-	 System.out.println("5: Apply Transformation");
-	 System.out.println("6: Viewport-spec");
-	 System.out.println("7: Display Pixels");
-	 System.out.println("8: Input Lines");
-	 System.out.println("9: Output Lines");
-	 System.out.println("10: Exit");
-
-	 int input = scan.nextInt();
-	 switch(input) {
-	 case 1:
-	     break;
-	 case 2:
-	     break;
-	 case 3:
-	     break;
-	 case 4:
-	     break;
-	 case 5:
-	     break;
-	 case 6:
-	     break;
-	 case 7:
-	     break;
-	 case 8:
-	     break;
-	 case 9:
-	     DataLine.outputLines(panel.getDatalines(), panel.getNumOfDatalines());
-	     break;
-	 case 10:
-	     break;
-	 default:
-	     System.out.println("Invalid input.");
-	 }
-	
 	 frame.add(panel);
 	 frame.pack();
 	 frame.setVisible(true);
 	 frame.setResizable(false);
 	 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	 while(isRunning) {
+	     System.out.println("What would you like to do?");
+	     System.out.println("1: Basic Translation");
+	     System.out.println("2: Basic Scale");
+	     System.out.println("3: Basic Rotate");
+	     System.out.println("4: Concatenate");
+	     System.out.println("5: Apply Transformation");
+	     System.out.println("6: Viewport-spec");
+	     System.out.println("7: Display Pixels");
+	     System.out.println("8: Input Lines");
+	     System.out.println("9: Output Lines");
+	     System.out.println("10: Exit");
+	     
+	     int input = scan.nextInt();
+	     switch(input) {
+	     case 1:
+		 break;
+	     case 2:
+		 break;
+	     case 3:
+		 break;
+	     case 4:
+		 break;
+	     case 5:
+		 break;
+	     case 6:
+		 break;
+	     case 7:
+		 break;
+	     case 8:
+		 break;
+	     case 9:
+		 DataLine.outputLines(panel.getDatalines(), panel.getNumOfDatalines());
+		 break;
+	     case 10:
+		 isRunning = false;
+		 break;
+	     default:
+		 System.out.println("Invalid input.");
+	     }
+	 }//while
+
+	 System.exit(0);
     }
 }
 
